3dbb57a0106eb8b01e2a9c2a0130f9347134f1d1
whx3000 <wanghaoxi3000@163.com>
2017-08-02 18:19:50 +0800

    添加一个Python的基本的爬虫代码

---
+++ Python/Spider/easy_spider/html_downloader.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from urllib import request
+
+
+class HtmlDownloader(object):
+
+    def download(self, url):
+        if url is None:
+            return None
+
+        response = request.urlopen(url)
+        if response.getcode() != 200:
+            return None
+
+        return response.read()
---
+++ Python/Spider/easy_spider/test_urllib.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""Python3中下载网页的三种办法"""
+
+from urllib import request
+import http.cookiejar
+
+
+url = 'http://www.baidu.com'
+
+print('第一种方法')
+response = request.urlopen(url)
+print(response.getcode())
+print(len(response.read()))
+
+print('第二种方法')
+req = request.Request(url)
+req.add_header('user-agent', 'Mozilla/5.0')
+response2 = request.urlopen(req)
+print(response2.getcode())
+print(len(response2.read()))
+
+print('第三种方法')
+cj = http.cookiejar.CookieJar()
+opener = request.build_opener(request.HTTPCookieProcessor(cj))
+request.install_opener(opener)
+response3 = request.urlopen(url)
+print(response3.getcode())
+print(cj)
+print(response3.read())
