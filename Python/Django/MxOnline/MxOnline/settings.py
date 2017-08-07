d6d8d3ad813569eba4eb6ef1d6949fe74553e00c
whx3000 <wanghaoxi3000@163.com>
2017-08-07 17:49:22 +0800

    修改View中函数处理的方式为类处理 添加form类处理表单 添加验证码第三方模块 添加注册功能及其所需的发送邮件，随机码生成功能

--- Python/Django/MxOnline/MxOnline/settings.py
+++ Python/Django/MxOnline/MxOnline/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 AUTHENTICATION_BACKENDS = (
-    'users.views.CunstomBackend',
+    'users.views.CustomBackend',
 )
 
 INSTALLED_APPS = [
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'operation',
     'xadmin',
     'crispy_forms',
+    'captcha',
 ]
 
 AUTH_USER_MODEL = 'users.UserProfile'
@@ -137,3 +138,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+EMAIL_HOST = 'smtp.sina.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'projectsedu@sina.com'
+EMAIL_HOST_PASSWORD = 'admin123'
+EMAIL_USE_TLS = False
+EMAIL_FROM = 'projectsedu@sina.com'
---
+++ Python/Django/MxOnline/apps/utils/email_send.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from random import Random
+from django.core.mail import send_mail
+
+from users.models import EmailVerifyRecord
+from MxOnline import settings
+
+
+def random_str(randomlength=8):
+    random_str = ''
+    chars = 'ABCDEFGHYJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    length = len(chars) - 1
+    random = Random()
+    for i in range(randomlength):
+        random_str += chars[random.randint(0,length)]
+    return random_str
+
+
+def send_register_email(email, send_type='register'):
+    email_record = EmailVerifyRecord()
+    code = random_str(16)
+    email_record.code = code
+    email_record.email = email
+    email_record.send_type = send_type
+    email_record.save()
+
+    email_title = ''
+    email_body = ''
+
+    if send_type == 'register':
+        email_title = '慕学在线注册激活链接'
+        email_body = '点击链接激活你的账号：http://127.0.0.1:8000/active/{}'.format(code)
+
+    send_status = send_mail(email_title, email_body, settings.EMAIL_FROM, [email])
+    if send_status:
+        pass
