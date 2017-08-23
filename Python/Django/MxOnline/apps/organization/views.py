c9fe9dd33423504439346ee6675d571bf3871a4c
whx3000 <wanghaoxi3000@163.com>
2017-08-24 00:28:41 +0800

    添加课程列表的分页功能

--- Python/Django/MxOnline/apps/organization/views.py
+++ Python/Django/MxOnline/apps/organization/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import CourseOrg, CityDict
 
@@ -14,8 +15,18 @@ class OrgView(View):
         org_nums = all_orgs.count()
         all_cities = CityDict.objects.all()
 
+        # 对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_orgs, 5, request=request)
+
+        orgs = p.page(page)
+
         return render(request, 'org-list.html', {
-            'all_orgs': all_orgs,
+            'all_orgs': orgs,
             'org_nums': org_nums,
             'all_cities': all_cities
         })
