7daffdb375aa355a4a606788f473fde1265170b3
whx3000 <wanghaoxi3000@163.com>
2017-09-06 14:40:28 +0800

    1.添加ES6模块化实例代码

--- JavaScript/base/ES6_Study/app/js/index.js
+++ JavaScript/base/ES6_Study/app/js/index.js
@@ -1,3 +1,9 @@
 // import 'babel-polyfill'
-import './class/set-map'
+// import './class/Iterator'
 
+// 模块化导入
+// import {A, test, Hello} from './class/moduel';
+// import * as moduel from './class/moduel'
+import Moduel from './class/moduel'
+
+console.log(Moduel.A)
