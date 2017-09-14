b098936afde58efaa739ac2eb266f8bca89eb33f
whx3000 <wanghaoxi3000@163.com>
2017-09-15 00:13:59 +0800

    vue入门练习：完善弹出窗口，添加登录和注册组件

--- JavaScript/vue/vueshop/src/components/layout.vue
+++ JavaScript/vue/vueshop/src/components/layout.vue
@@ -5,9 +5,9 @@
         <img src="../assets/logo.png">
         <div class="head-nav">
           <ul class="nav-list">
-            <li>登录</li>
+            <li @click="logClick">登录</li>
             <li class="nav-pile">|</li>
-            <li>注册</li>
+            <li @click="regClick">注册</li>
             <li class="nav-pile">|</li>
             <li @click="aboutClick">关于</li>
           </ul>
@@ -25,28 +25,53 @@
       <p>© 2016 fishenal MIT</p>
     </div>
 
-    <my-dialog :is-show="isShowDialog" @on-close="closeDialog">
-      <p>other Hello</p>
+    <my-dialog :is-show="isShowLogDialog" @on-close="closeDialog('isShowLogDialog')">
+      <log-form></log-form>
+    </my-dialog>
+
+    <my-dialog :is-show="isShowRegDialog" @on-close="closeDialog('isShowRegDialog')">
+      <reg-form></reg-form>
+    </my-dialog>
+
+    <my-dialog :is-show="isShowAboutDialog" @on-close="closeDialog('isShowAboutDialog')">
+      <p>本报告在调研数据的基础上，采用定性与定量相结合的方式深入分析了专车市场发展的驱动因素与阻碍因素、
+        专车市场背后的产业格局、专车企业的竞争格局、用户对专车市场的依赖程度、专车对其他交通工具运力的补充效应等，
+        通过这五个章节的研究反映专车市场的发展态势和面临的问题。报告力求客观、深入、准确地反映中国专车市场发展情况，
+        为政府、企事业单位和社会各界提供决策依据。</p>
     </my-dialog>
   </div>
 </template>
 
 <script>
 import Dialog from './dialog'
+import LogForm from './logForm'
+import RegForm from './regForm'
 export default {
   name: 'layout',
-  components: { myDialog: Dialog },
+  components: {
+    myDialog: Dialog,
+    LogForm: LogForm,
+    RegForm: RegForm
+  },
   methods: {
-    aboutClick () {
-      this.isShowDialog = true
+    logClick () {
+      this.isShowLogDialog = true
     },
-    closeDialog () {
-      this.isShowDialog = false
+    regClick () {
+      this.isShowRegDialog = true
+    },
+    aboutClick () {
+      this.isShowAboutDialog = true
+    },
+    closeDialog (attr) {
+      this[attr] = false
     }
   },
   data () {
     return {
-      isShowDialog: false
+      isShowAboutDialog: false,
+      isShowLogDialog: false,
+      isShowRegDialog: false
     }
   }
 }
