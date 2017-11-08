5d7f70c282330c612001a67ab630274ae493331a
whx3000 <wanghaoxi3000@163.com>
2017-11-08 22:15:43 +0800

    慕课网Django: 完善用户信息页的各项修改接口

--- Python/Django/MxOnline/apps/utils/email_send.py
+++ Python/Django/MxOnline/apps/utils/email_send.py
@@ -20,7 +20,10 @@ def random_str(randomlength=8):
 
 def send_register_email(email, send_type='register'):
     email_record = EmailVerifyRecord()
-    code = random_str(16)
+    if send_type == "update_email":
+        code = random_str(4)
+    else:
+        code = random_str(16)
     email_record.code = code
     email_record.email = email
     email_record.send_type = send_type
