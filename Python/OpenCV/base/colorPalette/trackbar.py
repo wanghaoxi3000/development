57faadaed083e7c41f7d146cdc516db34d74d53c
wanghaoxi3000 <wanghaoxi3000@163.com>
2018-01-03 10:41:27 +0800

    OpenCV添加轨迹按钮代码

---
+++ Python/OpenCV/base/colorPalette/trackbar.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+
+
+def nothing(x):
+    pass
+
+
+# 创建一个显示黑色图片的窗口
+img = np.zeros((300, 512, 3), np.uint8)
+cv2.namedWindow('image')
+
+# 创建调节颜色的轨迹条
+cv2.createTrackbar('R', 'image', 0, 255, nothing)
+cv2.createTrackbar('G', 'image', 0, 255, nothing)
+cv2.createTrackbar('B', 'image', 0, 255, nothing)
+
+# 创建功能开关
+switch = '0 : OFF \n1 : ON'
+cv2.createTrackbar(switch, 'image', 0, 1, nothing)
+
+while True:
+    cv2.imshow('image', img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('q'):
+        break
+
+    # 获取轨迹条的取值
+    r = cv2.getTrackbarPos('R', 'image')
+    g = cv2.getTrackbarPos('G', 'image')
+    b = cv2.getTrackbarPos('B', 'image')
+    s = cv2.getTrackbarPos(switch, 'image')
+
+    if s == 0:
+        img[:] = 0
+    else:
+        img[:] = [b, g, r]
+
+cv2.destroyAllWindows()
